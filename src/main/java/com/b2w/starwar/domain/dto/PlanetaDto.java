1f86c5094ba63be71302e440f7a00b37694e14b2
Marcos Pinho <marcosjava2008@gmail.com>
2020-02-01 18:02:34 -0200

    Ajustes na API para não exibir informações nulas no detalhamento do starwar.

--- src/main/java/com/b2w/starwar/domain/dto/PlanetaDto.java
+++ src/main/java/com/b2w/starwar/domain/dto/PlanetaDto.java
@@ -1,5 +1,6 @@
 package com.b2w.starwar.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,14 +18,19 @@ public class PlanetaDto implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String id;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String nome;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String clima;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String terreno;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Integer totalDeAparicoesEmFilmes;
 
 }
